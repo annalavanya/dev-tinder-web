5378b37f1a86b24363b0210853c11f3a7f371296
Anna Lavanya <annalavanya2002@gmail.com>
2025-10-16 17:29:00 +0530

    card changes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -35,40 +35,41 @@ const Profile = () => {
   }
 
   return (
-  <div>
-    <p className='flex justify-center mt-5 font-bold'>Update page</p>
-    <div className="flex justify-center flex-row my-6">
-      <div className="flex flex-col border border-gray-700 px-8 py-5">
-        <label className="label">First Name</label>
-        <input type="text" className="input mb-2" value={firstName} onChange={(e) => setFirstName(e.target.firstName)}/>
-        <label className="label">Last Name</label>
-        <input type="text" className="input mb-2" value={lastName} onChange={(e) => setLastName(e.target.lastName)}/>
-        <label className="label">Age</label>
-        <input type="number" className="input mb-2" value={age} onChange={(e) => setAge(e.target.age)}/>
-        <label className="label">Gender</label>
-        <input type="text" className="input mb-2" value={gender} onChange={(e) => setGender(e.target.gender)}/>
-        <label className="label">PhotoUrl</label>
-        <input type="text" className="input mb-2" value={photoUrl} onChange={(e) => setPhotoUrl(e.target.photoUrl)}/>
-        <label className="label">About</label>
-        <input type="text" className="input mb-2" value={about} onChange={(e) => setAbout(e.target.about)}/>
-        <label className="label">Skills</label>
-        <input type="text" className="input mb-4" value={skills} onChange={(e) => setSkills(e.target.skills)}/>
-        <button className='flex justify-center bg-emerald-500 p-2 rounded-2xl w-24 cursor-pointer' onClick={updateUser}>Update</button>
-      </div>
+    <div>hello</div>
+  // <div>
+  //   <p className='flex justify-center mt-5 font-bold'>Update page</p>
+  //   <div className="flex justify-center flex-row my-6">
+  //     <div className="flex flex-col border border-gray-700 px-8 py-5">
+  //       <label className="label">First Name</label>
+  //       <input type="text" className="input mb-2" value={firstName} onChange={(e) => setFirstName(e.target.firstName)}/>
+  //       <label className="label">Last Name</label>
+  //       <input type="text" className="input mb-2" value={lastName} onChange={(e) => setLastName(e.target.lastName)}/>
+  //       <label className="label">Age</label>
+  //       <input type="number" className="input mb-2" value={age} onChange={(e) => setAge(e.target.age)}/>
+  //       <label className="label">Gender</label>
+  //       <input type="text" className="input mb-2" value={gender} onChange={(e) => setGender(e.target.gender)}/>
+  //       <label className="label">PhotoUrl</label>
+  //       <input type="text" className="input mb-2" value={photoUrl} onChange={(e) => setPhotoUrl(e.target.photoUrl)}/>
+  //       <label className="label">About</label>
+  //       <input type="text" className="input mb-2" value={about} onChange={(e) => setAbout(e.target.about)}/>
+  //       <label className="label">Skills</label>
+  //       <input type="text" className="input mb-4" value={skills} onChange={(e) => setSkills(e.target.skills)}/>
+  //       <button className='flex justify-center bg-emerald-500 p-2 rounded-2xl w-24 cursor-pointer' onClick={updateUser}>Update</button>
+  //     </div>
 
-      <div className='flex flex-col border border-gray-700 px-8 py-5'>
-        <div className='flex justify-center'>
-          <img src={photoUrl} alt='image' className='w-48 mt-4 rounded-2xl'/>
-        </div>
-        <p className='text-center font-bold p-1'>{firstName + " " + lastName}</p>
-        <p className='text-center font-bold p-1'>{age}</p>
-        <p className='text-center font-bold p-1'>{gender}</p>
-        { about && <p className='text-xs ml-3 justify-normal'>{about}</p>}
-        { skills && <p>{skills.join(', ')}</p>}
-      </div>
+  //     <div className='flex flex-col border border-gray-700 px-8 py-5'>
+  //       <div className='flex justify-center'>
+  //         <img src={photoUrl} alt='image' className='w-48 mt-4 rounded-2xl'/>
+  //       </div>
+  //       <p className='text-center font-bold p-1'>{firstName + " " + lastName}</p>
+  //       <p className='text-center font-bold p-1'>{age}</p>
+  //       <p className='text-center font-bold p-1'>{gender}</p>
+  //       { about && <p className='text-xs ml-3 justify-normal'>{about}</p>}
+  //       { skills && <p>{skills.join(', ')}</p>}
+  //     </div>
 
-    </div>
-  </div>
+  //   </div>
+  // </div>
   )
 }
 
